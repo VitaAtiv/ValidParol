e6b9f9f507bb0313f8cd76c58860fe3b05a91e0b
VitaAtiv <142582763+VitaAtiv@users.noreply.github.com>
2024-03-03 21:18:59 +0200

    add

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const isValidParol = document.querySelector(".text-parol-input");
 
 function parolValid(event) {
-    console.log(event.target);
-
+   
+    const parolEl = event.target.value;
+    const nonEnglishRegex = /[^A-Za-z]/;
+    const isNotValidEng = nonEnglishRegex.test(parolEl);
+    if (isNotValidEng) {
+         isValidParol.classList.add("not-valid");
+    }
+    const firstLiter = event.target.value[0]
+    console.log(firstLiter);
+    const nonFirstLiter = /^[A-Z]/;
+    const isNotValidFirstLiterBig = nonFirstLiter.test(firstLiter);
 }
 
-isValidParol.addEventListener ("input", parolValid)
+
+    isValidParol.addEventListener("input", parolValid)
